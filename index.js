97f72620a27cff9d8e1edd5a5bb5d8ccf885168a
Darlan Alves <me@darlanalv.es>
2020-09-07 20:22:40 +0200

    feat: add json validation action

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-import { lambda } from '@node-lambdas/core';
+import { lambda, Format } from '@node-lambdas/core';
+import Ajv from 'ajv';
 
 const configutation = {
   version: 2,
   actions: {
-    echo: {
+    schemaAndData: {
       default: true,
-      handler: (input, output) => input.pipe(output),
-    }
-  }
+      input: Format.Json,
+      output: Format.Json,
+      handler: (input, output) => {
+        const validator = new Ajv({ allErrors: true, schemaId: 'auto' });
+        const { schema, data } = input.body;
+
+        validator.validate(schema, data);
+        output.send(validator.errors);
+      },
+    },
+  },
 };
 
 lambda(configutation);
